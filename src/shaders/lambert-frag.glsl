8d049411248c918427d79b13bd0fc924a7046668
RFK <kakigori11235@gmail.com>
2025-09-22 23:10:07 -0400

    main implementations

---
+++ src/shaders/lambert-frag.glsl
@@ -0,0 +1,171 @@
+#version 300 es
+
+// This is a fragment shader. If you've opened this file first, please
+// open and read lambert.vert.glsl before reading on.
+// Unlike the vertex shader, the fragment shader actually does compute
+// the shading of geometry. For every pixel in your program's output
+// screen, the fragment shader is run for every bit of geometry that
+// particular pixel overlaps. By implicitly interpolating the position
+// data passed into the fragment shader by the vertex shader, the fragment shader
+// can compute what color to apply to its pixel based on things like vertex
+// position, light position, and vertex color.
+precision highp float;
+#define PI 3.1415926535
+
+uniform vec4 u_Color; // The color with which to render this instance of geometry.
+uniform float u_Time;
+
+// These are the interpolated values out of the rasterizer, so you can't know
+// their specific values without knowing the vertices that contributed to them
+in vec3 fs_Nor;
+in vec4 fs_LightVec;
+in vec4 fs_Col;
+in vec3 fs_Pos;
+
+out vec4 out_Col; // This is the final output color that you will see on your
+                  // screen for the pixel that is currently being processed.
+float random2(vec2 st) {
+    return fract(sin(dot(st, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+float perlin2D(vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    float a = random2(i);
+    float b = random2(i + vec2(1.0, 0.0));
+    float c = random2(i + vec2(0.0, 1.0));
+    float d = random2(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    float x1 = mix(a, b, u.x);
+    float x2 = mix(c, d, u.x);
+
+    return mix(x1, x2, u.y);
+}
+
+// Value noise 3D by iq
+// https://www.shadertoy.com/view/4sfGzS
+float hash( ivec3 p )    // this hash is not production ready, please
+{                        // replace this by something better
+
+    // 3D -> 1D
+    int n = p.x*3 + p.y*113 + p.z*311;
+
+    // 1D hash by Hugo Elias
+	n = (n << 13) ^ n;
+    n = n * (n * n * 15731 + 789221) + 1376312589;
+    return float( n & ivec3(0x0fffffff))/float(0x0fffffff);
+}
+
+float valueNoise3D( in vec3 x )
+{
+    ivec3 i = ivec3(floor(x));
+    vec3 f = fract(x);
+    f = f*f*(3.0-2.0*f);
+	
+    return mix(mix(mix( hash(i+ivec3(0,0,0)), 
+                        hash(i+ivec3(1,0,0)),f.x),
+                   mix( hash(i+ivec3(0,1,0)), 
+                        hash(i+ivec3(1,1,0)),f.x),f.y),
+               mix(mix( hash(i+ivec3(0,0,1)), 
+                        hash(i+ivec3(1,0,1)),f.x),
+                   mix( hash(i+ivec3(0,1,1)), 
+                        hash(i+ivec3(1,1,1)),f.x),f.y),f.z);
+}
+
+float fbm(vec3 p) {
+
+    // PARAMETERS
+    int iterCount = 3; 
+    float ampDecreaseFactor = 0.5;
+    float freqIncreaseFactor = 2.;
+    float amp = 0.5; // INITIAL AMP
+
+    // base values
+    float fbmSum = 0.;
+    vec3 seed = p;
+    
+    for (int i = 0; i < iterCount; i++) {
+
+        fbmSum += valueNoise3D(seed) * amp;
+
+        // decreasing factors
+        amp *= ampDecreaseFactor;
+        seed *= freqIncreaseFactor;
+    }
+    return fbmSum;
+}
+
+vec3 getGradColor(float t) {
+    // a + b * cos(2 * PI * (c * t + d));
+
+    // // 1. ORANGE~BLUE
+    // vec3 offset = vec3(0.5, 0.5, 0.5);
+    // vec3 amp = vec3(0.5, 0.5, 0.5);
+    // vec3 c = vec3(1, 1, 1);
+    // vec3 d = vec3(0, 0.10, 0.20);
+
+    // 4. LEGO
+    // vec3 offset = vec3(-1.000, -1.000, -1.000);
+    // vec3 amp = vec3(31.384, 31.384, 942.797);
+    // vec3 c = vec3(2.107, 2.107, 2.107);
+    // vec3 d = vec3(-2.275, -1.941, -1.608);
+
+    // [[0.646 0.361 0.266] [0.739 0.558 0.597] [0.733 0.588 0.485] [0.907 1.837 -1.450]]
+    // vec3 offset = vec3(0.646, 0.361, 0.266);
+    // vec3 amp    = vec3(0.739, 0.558, 0.597);
+    // vec3 c      = vec3(0.733, 0.588, 0.485);
+    // vec3 d      = vec3(0.907, 1.837, -1.450);
+
+    // red
+    // vec3 offset = vec3(0.000, 0.500, 0.500);
+    // vec3 amp    = vec3(0.000, 0.500, 0.500);
+    // vec3 c      = vec3(0.000, 0.500, 0.333);
+    // vec3 d      = vec3(0.000, 0.500, 0.667);
+
+    // vec3 offset = vec3(0.678, 0.378, 0.048);
+    // vec3 amp    = vec3(0.848, 1.108, 0.038);
+    // vec3 c      = vec3(-0.382, 0.328, 0.333);
+    // vec3 d      = vec3(0.500, -1.502, 0.667);
+    
+    // blue
+    // vec3 offset = vec3(0.678, 0.378, 0.048);
+    // vec3 amp    = vec3(0.848, 1.108, 0.038);
+    // vec3 c      = vec3(-0.382, 0.588, 0.333);
+    // vec3 d      = vec3(0.368, -1.633, 0.535);
+    
+    // magenta
+    vec3 offset = vec3(0.938, 0.328, 0.718);
+    vec3 amp    = vec3(0.659, 0.438, 0.328);
+    vec3 c      = vec3(0.388, 0.388, 0.296);
+    vec3 d      = vec3(2.538, 2.478, 0.168);
+
+    float r0 = offset.r + amp.r * cos(2. * PI * (c.r * t + d.r));
+    float g0 = offset.g + amp.g * cos(2. * PI * (c.g * t + d.g));
+    float b0 = offset.b + amp.b * cos(2. * PI * (c.b * t + d.b));
+
+
+    return vec3(r0, g0, b0);
+}
+
+void main() {
+	
+  vec3 color = vec3(0.5 * (fs_Pos.xy + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0));
+  vec2 seed = vec2(fs_Pos.x, fs_Pos.y) * 5.;
+  //color = color * abs(dot(fs_Nor, fs_LightVec));
+  // float colorNoise = perlin2D(fs_Pos.xy * 2. + u_Time * 0.01);
+  float baseNoise = fbm(vec3(seed.x, seed.y - u_Time * 0.01, 0.));
+  float topNoise = baseNoise * fs_Pos.y;
+  
+  color = 1. - getGradColor(length(fs_Pos.xy) + u_Time * 0.001 + topNoise * 0.5);
+
+  // discard top
+  // float topNoise = 1. - perlin2D(vec2(seed.x, seed.y - u_Time * 0.01)) * fs_Pos.y;
+  
+  if (topNoise > 0.5) {discard;}
+  //if (baseNoise * length(fs_Pos) > 0.5) {discard;}
+
+  out_Col = vec4(color, 1.);
+}
---
+++ src/shaders/lambert-vert.glsl
@@ -0,0 +1,150 @@
+#version 300 es
+
+//This is a vertex shader. While it is called a "shader" due to outdated conventions, this file
+//is used to apply matrix transformations to the arrays of vertex data passed to it.
+//Since this code is run on your GPU, each vertex is transformed simultaneously.
+//If it were run on your CPU, each vertex would have to be processed in a FOR loop, one at a time.
+//This simultaneous transformation allows your program to run much faster, especially when rendering
+//geometry with millions of vertices.
+
+uniform mat4 u_Model;       // The matrix that defines the transformation of the
+                            // object we're rendering. In this assignment,
+                            // this will be the result of traversing your scene graph.
+
+uniform mat4 u_ModelInvTr;  // The inverse transpose of the model matrix.
+                            // This allows us to transform the object's normals properly
+                            // if the object has been non-uniformly scaled.
+
+uniform mat4 u_ViewProj;    // The matrix that defines the camera's transformation.
+                            // We've written a static matrix for you to use for HW2,
+                            // but in HW3 you'll have to generate one yourself
+uniform float u_Time;
+
+in vec4 vs_Pos;             // The array of vertex positions passed to the shader
+
+in vec4 vs_Nor;             // The array of vertex normals passed to the shader
+
+in vec4 vs_Col;             // The array of vertex colors passed to the shader.
+
+out vec3 fs_Nor;            // The array of normals that has been transformed by u_ModelInvTr. This is implicitly passed to the fragment shader.
+out vec4 fs_LightVec;       // The direction in which our virtual light lies, relative to each vertex. This is implicitly passed to the fragment shader.
+out vec4 fs_Col;            // The color of each vertex. This is implicitly passed to the fragment shader.
+out vec3 fs_Pos;
+
+
+float random3(vec3 st) {
+    return fract(sin(dot(st, vec3(12.9898,78.233, 37.719))) * 43758.5453123);
+}
+
+float value3D(vec3 st) {
+    vec3 i = floor(st);
+    vec3 f = fract(st);
+
+    // cube
+    float a = random3(i);
+    float b = random3(i + vec3(1.0, 0.0, 0.0));
+    float c = random3(i + vec3(0.0, 1.0, 0.0));
+    float d = random3(i + vec3(1.0, 1.0, 0.0));
+    float e = random3(i + vec3(0.0, 0.0, 1.0));
+    float f1 = random3(i + vec3(1.0, 0.0, 1.0));
+    float g = random3(i + vec3(0.0, 1.0, 1.0));
+    float h = random3(i + vec3(1.0, 1.0, 1.0));
+
+    // hermite
+    vec3 u = f * f * (3.0 - 2.0 * f);
+
+    float x1 = mix(a, b, u.x);
+    float x2 = mix(c, d, u.x);
+    float x3 = mix(e, f1, u.x);
+    float x4 = mix(g, h, u.x);
+
+    float y1 = mix(x1, x2, u.y);
+    float y2 = mix(x3, x4, u.y);
+
+    return mix(y1, y2, u.z);
+}
+
+vec3 randomGradient(vec3 p) {
+    float x = fract(sin(dot(p, vec3(127.1, 311.7, 74.7))) * 43758.5453);
+    float y = fract(sin(dot(p, vec3(269.5,183.3,246.1))) * 43758.5453);
+    float z = fract(sin(dot(p, vec3(113.5,271.9,124.6))) * 43758.5453);
+    return normalize(vec3(x - 0.5, y - 0.5, z - 0.5));
+}
+
+float perlin3D(vec3 st) {
+    vec3 i = floor(st);
+    vec3 f = fract(st);
+
+    vec3 u = f * f * f * (f * (f * 6.0 - 15.0) + 10.0);
+
+    // 8 corners
+    float n000 = dot(randomGradient(i + vec3(0,0,0)), f - vec3(0,0,0));
+    float n100 = dot(randomGradient(i + vec3(1,0,0)), f - vec3(1,0,0));
+    float n010 = dot(randomGradient(i + vec3(0,1,0)), f - vec3(0,1,0));
+    float n110 = dot(randomGradient(i + vec3(1,1,0)), f - vec3(1,1,0));
+    float n001 = dot(randomGradient(i + vec3(0,0,1)), f - vec3(0,0,1));
+    float n101 = dot(randomGradient(i + vec3(1,0,1)), f - vec3(1,0,1));
+    float n011 = dot(randomGradient(i + vec3(0,1,1)), f - vec3(0,1,1));
+    float n111 = dot(randomGradient(i + vec3(1,1,1)), f - vec3(1,1,1));
+
+    // interpolate
+    float nx00 = mix(n000, n100, u.x);
+    float nx10 = mix(n010, n110, u.x);
+    float nx01 = mix(n001, n101, u.x);
+    float nx11 = mix(n011, n111, u.x);
+
+    float nxy0 = mix(nx00, nx10, u.y);
+    float nxy1 = mix(nx01, nx11, u.y);
+
+    return mix(nxy0, nxy1, u.z);
+}
+
+float customFBM(vec3 st) {
+    
+    float speed = 0.001;
+    // LEVEL 1
+    float freq = 1.;
+    float amp = 3.; 
+    vec3 seed = vec3(st.xy, st.z + u_Time * speed) * freq;
+    float baseNoise1 =  perlin3D(seed);
+    baseNoise1 = baseNoise1 * .5 + .5; // 0 to 1
+    baseNoise1 *= amp;
+    
+    // LEVEL 2
+    speed *= 10.;
+    freq *= 2.0;
+    amp *= 0.1;
+    seed = vec3(st.yz, st.x + u_Time * speed) * freq;
+    float Noise2 = 0.;//perlin3D(seed) * amp;
+
+
+
+    return baseNoise1 + Noise2;
+}
+
+void main() {
+    vec3 lightPos = normalize(vec3(0.0, 10.0, 10.0));
+    
+    // pos
+    vec4 localPos = vs_Pos;
+    // offsets -----------------
+    
+    // general stretch in upper dir
+    localPos.y += vs_Nor.y * customFBM(localPos.xyz) * max(0., localPos.y);
+    localPos.xz += sin(u_Time * 0.01 + localPos.y) * 0.15 * max(-0.25, localPos.y);
+    
+    
+    localPos.xyz -= vs_Nor.xyz * (vs_Pos.y * 0.5);
+    
+    //localPos += (sin(u_Time));
+    fs_Pos = localPos.xyz;
+    // end of offsets -------------
+    // nor
+    mat3 invTranspose = mat3(u_ModelInvTr);
+    fs_Nor = invTranspose * vec3(vs_Nor);  
+    // light vec
+    vec4 modelposition = u_Model * localPos; 
+    // fs_LightVec = lightPos - modelposition.xyz;
+    
+    gl_Position = u_ViewProj * modelposition;
+}
